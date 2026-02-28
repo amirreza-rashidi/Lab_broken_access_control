1c077a5b26e4213aa3300d89d671509e20d6b01f
Amirreza Rashidi <amirrezarashidi5831ar@gmail.com>
2026-02-28 03:57:02 +0330

    problems solved!

--- server.php
+++ server.php
@@ -1,11 +1,11 @@
 <?php 
 $user_name = $_POST['username'];
 $password = $_POST['password'];
-$isAdmin   = $_POST['isAdmin'];
+$isAdmin = $_POST['isAdmin'] ?? null;
 
 if ($user_name=="admin" && $password=="admin"){echo "200";}
 
-else if ($isAdmin == "true"){echo "200";}
+else if ($isAdmin == "true"){echo "202";}
 else{
     echo "201";}
 
